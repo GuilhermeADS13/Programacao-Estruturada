92851684049dc883d91b79807fbd4ef75a8dd647
Guilherme Maciel <lakilleo@outlook.com>
2026-08-31 14:49:05 -0300

    Adiciona produto escalar de vetores (ex2.c) e config do VS Code

---
+++ ex2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float vector_dot(float* values1, float* values2, unsigned int n);
+float* vector_read(unsigned int n);
+
+int main(){
+    unsigned int n;
+    scanf("%u", &n);
+    float* vetor1 = vector_read(n);
+    float* vetor2 = vector_read(n);
+
+    float dot = vector_dot(vetor1, vetor2, n);
+
+    printf("%f", dot);
+
+    free(vetor1);
+    free(vetor2);
+
+    return 0;
+}
+
+float* vector_read(unsigned int n){
+    float* values = malloc(sizeof(float) * n);
+    for(unsigned int i = 0; i < n; i++){
+        scanf("%f", values + i);
+    }
+    return values;
+}
+
+float vector_dot(float* values1, float* values2, unsigned int n){
+    float dot = 0;
+    for(unsigned int i = 0; i < n; i++){
+        dot += values1[i] * values2[i];
+    }
+    return dot;
+}
